3de6478b2db557774c5af32a017b64a6b17e6eca
Max <mr.madu@ya.ru>
2021-03-03 21:22:04 +0300

    les65

--- js/script.js
+++ js/script.js
@@ -365,11 +365,15 @@ window.addEventListener('DOMContentLoaded', function() {
         dots.push(dot);
     }
 
+    function getWidth (width){
+        return +width.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if (offset == getWidth(width) * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += getWidth(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -387,9 +391,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
     prev.addEventListener('click', () => {
         if (offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = getWidth(width) * (slides.length - 1);
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += getWidth(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -410,7 +414,7 @@ window.addEventListener('DOMContentLoaded', function() {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = getWidth(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
